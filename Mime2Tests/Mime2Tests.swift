2f27eed333dc8a6bad3e4d983ee9cc9d1060a996
William Judd <wjiv33@gmail.com>
2015-10-18 17:11:33 -0400

    Initial Commit

---
+++ Mime2Tests/Mime2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Mime2Tests.swift
+//  Mime2Tests
+//
+//  Created by William Judd on 10/18/15.
+//  Copyright © 2015 William Judd. All rights reserved.
+//
+
+import XCTest
+@testable import Mime2
+
+class Mime2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
